ec5929428a78a3e4d8b06b348cd2a748b852f048
Rob Bronson <robert.j.bronson@gmail.com>
2017-08-21 14:39:10 -0700

    added constructor for image object

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+//Constructor function for image object
+function Product() {
+
+  this.name = '';
+  this.id = '';
+  this.imgSource = '';
+  this.timesShown = 0;
+  this.timesClicked = 0;
+}
+
+var photos = [];
+//randomize photos
+
+
+//event listener and handler for clicking on photos. (one for each photo?)
