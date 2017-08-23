e81b1d5b246f118dd40448503ad07f724df8ee1c
Rob Bronson <robert.j.bronson@gmail.com>
2017-08-22 23:05:18 -0700

    Added graphs. sounds easier than it is.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var products = [];
+var productAvgTimesClicked = [];
+var productsSelected = [];
+var productsShown = [];
+var productsNotShown = [];
 var totalClicks = 0;
 var clickedId;
 var priorPics = [];
 var currentPics = [];
 var numSelectionsAllowed = 5;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
 //Constructor function for image object
 
@@ -49,10 +55,8 @@ function imageSwap(){
   console.log(priorPics);
   currentPics = [];
 
-//see if image was posted before
-
-
 }
+imageSwap();
 
 var image1 = document.getElementById('slot0');
 var image2 = document.getElementById('slot1');
@@ -81,16 +85,94 @@ function voteForPic(potato) {
     image2.removeEventListener('click', voteForPic);
     image3.removeEventListener('click', voteForPic);
     console.log('Done clicking');
-    report();
+    getChartData();
+    doTheChart();
   }
 }
-imageSwap();
-// function report() {
-//   for (var x = 0; x < products.length; x ++){
-//     document.write(products[x].name + '<br>');
-//     document.write('Shown: ' + products[x].timesShown + '<br>');
-//     document.write('Clicked: ' + products[x].timesClicked + '<br>');
-//     document.write('<br>');
-//     document.write('<br>');
-//   }
-// }
+
+/// TIme for some Chart JS!!\
+
+function getChartData() {
+  productAvgTimesClicked = [];
+  productsSelected = [];
+  productsShown = [];
+  productsNotShown = [];
+  for (var i = 0; i < products.length; i++){
+    if (products[i].timesShown > 0){
+      productsShown.push(products[i]);
+      productAvgTimesClicked.push(products[i].timesClicked / products[i].timesShown);
+      productsSelected.push(products[i]);
+    }
+    else {
+      productsNotShown.push(products[i]);
+    }
+  }
+}
+
+function doTheChart(){
+
+  var chartData =  {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'Times Selected',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: 'Times Shown',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  };
+  for (var i = 0; i < productsSelected.length; i++){
+    chartData.data.labels[i] = productsSelected[i].name;
+    // debugger;
+    chartData.data.datasets[0].data[i] = productsSelected[i].timesClicked;
+    chartData.data.datasets[1].data[i] = productsSelected[i].timesShown;
+  }
+  var myChart = new Chart(ctx, chartData);
+}
